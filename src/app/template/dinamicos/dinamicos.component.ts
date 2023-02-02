e011a969fd23c1cf5dc912cb92a0ac4115a2334b
Jesus Alcantara <jesus.alcantara@alten.es>
2023-02-02 13:33:02 +0100

    · Add section switches

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +17,34 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Jesus',
+    favoritos: [
+      { id: 1, nombre: 'DB Kakarot' },
+      { id: 2, nombre: 'League of Legends' },
+      { id: 3, nombre: 'Warframe' }
+    ]
+  }
+
+  guardar () {
+    console.log('Guardado con éxito!');
+  }
+
+  eliminar (index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+  agregarJuego () {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+
+    this.persona.favoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = '';
   }
 
 }
